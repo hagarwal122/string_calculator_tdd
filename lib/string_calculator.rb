ab2f3c1ceec0ecacc979173ae5bb2801afcb064b
Harshit <h.agarwal122@gmail.com>
2025-05-29 19:47:40 +0530

    Added case with \n and solution

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,8 @@
 class StringCalculator
   def self.add(input)
     return 0 if input.empty? 
-    num_arr = input.split(",").map(&:to_i)
+    match = /,|\n/
+    num_arr = input.split(match).map(&:to_i)
     num_arr.sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,6 +13,14 @@ RSpec.describe StringCalculator do
     it 'returns the sum of two numbers separated by comma' do
       expect(StringCalculator.add("1,5")).to eq(6)
     end
+
+    it 'returns the sum of multiple comma-separated numbers' do
+      expect(StringCalculator.add("1,2,3,4")).to eq(10)
+    end
+
+    it 'handles newlines as delimiters' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
     
   end
 end
