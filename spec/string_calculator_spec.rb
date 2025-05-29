91ace4857b6a551f94c7ad3599e80324b7eeaf0a
Harshit <h.agarwal122@gmail.com>
2025-05-29 17:20:09 +0530

    Added 2 more test cases now total 3

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,14 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it 'returns the number itself for a single number' do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
+
+    it 'returns the sum of two numbers separated by comma' do
+      expect(StringCalculator.add("1,5")).to eq(6)
+    end
+    
   end
 end
