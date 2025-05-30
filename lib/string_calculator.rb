3cee664b0e5e44df17663927e5fac36cce311b7d
Harshit <h.agarwal122@gmail.com>
2025-05-30 13:01:20 +0530

    Added case with // custom delimiter with solution

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,16 @@
 class StringCalculator
   def self.add(input)
     return 0 if input.empty? 
-    match = /,|\n/
-    num_arr = input.split(match).map(&:to_i)
+    delimiter = /,|\n/
+    numbers_str = input
+
+    if input.start_with?("//")
+      parts = input.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2])
+      numbers_str = parts[1]
+    end
+
+    num_arr = numbers_str.split(/#{delimiter}|,|\n/).map(&:to_i)
     num_arr.sum
   end
 end
